3f21b4584f1c6808ad727440e11b8f5f029aba0a
GoatGear <edgrdoierr@gmail.com>
2024-07-27 17:51:11 -0600

    Monguito

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://edgardoierr:R9r8Heojd0cwmO6V@mongodb1.ni1xbm8.mongodb.net/');
+        await mongoose.connect('monguit');
         console.log("Ready")
     } catch (error) {
         console.log(error);
