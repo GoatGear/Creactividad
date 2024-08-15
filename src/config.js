e234702caa5580b8c973957d4854ee7c9a554b24
GoatGear <edgrdoierr@gmail.com>
2024-08-15 05:51:48 -0600

    ultima para deploy

--- src/config.js
+++
@@ -1,3 +0,0 @@
-export const TOKEN_SECRET = 'some secret'
-export const apikey = 're_A1TshSHw_HYscKvTCAiq17j38T8Cv54Wm'
-export const MONGO_API_K = 'mongodb+srv://edgardoierr:R9r8Heojd0cwmO6V@mongodb1.ni1xbm8.mongodb.net/'
