9a7f798f3c69b1df426d77cfb44ea04d131a797e
GoatGear <edgrdoierr@gmail.com>
2024-08-19 15:23:03 -0600

    Ready para deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import {PORT} from '../src/config.js'
 import app from './app.js'
 import { connectDB } from './db.js'
 
 connectDB();
-app.listen(4000)
-console.log('Servidor inicializado en puerto', 4000)
+app.listen(PORT)
+console.log('Servidor inicializado en puerto', PORT)
