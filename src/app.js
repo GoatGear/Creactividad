7c96300560bf4d3b0220c06f68ac35f0c20920dd
GoatGear <edgrdoierr@gmail.com>
2024-08-23 19:08:29 -0600

    LOCALHOST
    Co-authored-by: Sergio A. Trejo <TrejoCode@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import cookieParser from 'cookie-parser';
+// import cookieParser from 'cookie-parser';
 import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
@@ -12,11 +12,10 @@ const app = express();
 app.use(cors({
     //origin: ['https://eleddie.com', 'https://www.eleddie.com'],
     origin: '*',
-    credentials: true,
 }));
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cookieParser());
+// app.use(cookieParser());
 
 app.use('/api', authRoutes);
 app.use('/api', rsvpRoutes);
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -60,7 +60,6 @@ export const register = async (req, res) => {
 
         // Crea el token y envía la respuesta al cliente
         const token = await createAccessToken({ id: userSaved._id, role: userSaved.role });
-        res.cookie('token', token);
         res.json({
             id: userSaved._id,
             nombre: userSaved.nombre,
@@ -69,6 +68,7 @@ export const register = async (req, res) => {
             especialidad: userSaved.especialidad,
             correo: userSaved.correo,
             role: userSaved.role,
+            token: token, // token
             createdAt: userSaved.createdAt,
             updatedAt: userSaved.updatedAt
         });
@@ -91,7 +91,6 @@ export const login = async (req, res) => {
         if (!isMatch) return res.status(400).json({ message: "Contraseña incorrecta" });
 
         const token = await createAccessToken({ id: userFound._id, role: userFound.role });
-        res.cookie('token', token)
         res.json({
             id: userFound._id,
             nombre: userFound.nombre,
@@ -99,6 +98,7 @@ export const login = async (req, res) => {
             especialidad: userFound.especialidad,
             correo: userFound.correo,
             role: userFound.role,
+            token: token, // token
             createdAt: userFound.createdAt,
             updatedAt: userFound.updatedAt
         });
@@ -111,6 +111,7 @@ export const logout = (req, res) => {
     res.cookie('token', "", {
         expires: new Date(0)
     })
+    
     return res.sendStatus(200);
 };
 
@@ -133,7 +134,8 @@ export const profile = async (req, res) => {
 }
 
 export const verifyToken = async (req, res) => {
-    const { token } = req.cookies
+    console.info(req.headers);
+    const { token } = req.headers;
     if (!token) return res.status(401).json({ message: "No autorizado no tok" });
     jwt.verify(token, TOKEN_SECRET, async (err, user) => {
         if (err) return res.status(401).json({ message: 'No autorizado' });
